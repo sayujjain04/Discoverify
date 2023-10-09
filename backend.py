a95f8198607c3d2bcc7e16535e5eaef3025bae43
sjanga2 <116621881+sjanga2@users.noreply.github.com>
2023-10-09 16:45:39 -0500

    Update backend.py-- Spotify OAuth
    Signed-off-by: sjanga2 <116621881+sjanga2@users.noreply.github.com>

--- backend.py
+++ backend.py
@@ -10,3 +10,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import spotifyAPI
 import textwrap, random
+
+
+# more imports for the spotify api
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+# api requests
+
+# received from the spotify developer account
+client_id = '80d232eeaa064241b531967dfaccb515'
+client_secret = '6b5084a5a9834bcb974bfb4d6c2a96d9'
+
+
+os.environ["SPOTIPY_CLIENT_ID"] = client_id
+os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
+os.environ["SPOTIPY_REDIRECT_URI"] = "https://open.spotify.com/"
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
